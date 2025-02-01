ed3b11cd5a31b6e10b0f8c854f8318bee39a46d4
superderped <24651666+DipRRai@users.noreply.github.com>
2025-02-01 17:35:08 +1100

    update

---
+++ app/(tabs)/sales.tsx
@@ -0,0 +1,46 @@
+import { Image, StyleSheet, View, TextInput, Text } from 'react-native';
+import { useState, useEffect } from 'react';
+
+//CUSTOM IMPORTS
+import AuctionItem from '../components/AuctionItem';
+
+const url = 'http://localhost:3000';
+
+async function getProducts() {
+    const response = await fetch(url);
+    const data = await response.json()
+    console.log(data);
+    return data
+}
+
+export default function Sales() {
+    const [text, onChangeText] = useState('Search on WebAuctions');
+    const [products, setProducts] = useState(null);
+
+    return (
+        <View style={styles.container}>
+            <Text >WebAuctions</Text>
+            <TextInput
+                style={styles.input}
+                onChangeText={onChangeText}
+                value={text}
+                onFocus={() => onChangeText('')}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        paddingTop: '5%',
+    },
+    input: {
+        height: 40,
+        width: '90%',
+        margin: 12,
+        borderWidth: 1,
+        padding: 10,
+    },
+});
---
+++ app/components/AuctionItem.tsx
@@ -0,0 +1,22 @@
+import { View, StyleSheet, Text } from 'react-native';
+
+type Props = {
+    name: string;
+};
+
+export default function AuctionItem({ name }: Props) {
+    return (
+        <View style={styles.listing}>
+            <Text style={{ color: '#fff' }}>{name}</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    listing: {
+        backgroundColor: '#121110',
+        width: '100%',
+        height: 140,
+        marginBottom: 10,
+    }
+})
